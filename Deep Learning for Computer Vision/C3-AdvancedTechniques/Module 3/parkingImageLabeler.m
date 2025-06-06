f7c910bfc6bc4e0962b64c6be003e1a22a329b56
Bishal Pandey <118913849+Prakashbishal@users.noreply.github.com>
2025-06-06 21:05:14 +0530

    Added Project Files

---
+++ Deep Learning for Computer Vision/C3-AdvancedTechniques/Module 3/parkingImageLabeler.m
@@ -0,0 +1,20 @@
+% Write an automation function for the parking images
+% using the detector from parkingYoloXDetectorSolution.mat
+%
+% Remember to:
+% - Align the function name with the file name
+% - Use a persistent variable for the detector for greater efficiency
+%
+% For each input image, save the output as a structure with 
+%    the following fields for each label type:
+% - "Name" containing the label name
+% - "Type" containing the label type/shape
+% - "Position" containing all bounding boxes with that label
+% 
+% If you get stuck, refer to FastenersImageLabeler.m for an example
+
+function autoLabels = parkingImageLabeler(I)
+% Add your code here
+
+
+end
